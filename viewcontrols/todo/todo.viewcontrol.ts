db7cd49d48964e971216423e911ed5c43e514d65
Darion Welch <darion.welch@gmail.com>
2014-09-16 11:53:17 -0500

    Lesson 3 - Adding functionality to our to-do list

--- viewcontrols/todo/todo.viewcontrol.ts
+++ viewcontrols/todo/todo.viewcontrol.ts
@@ -2,8 +2,21 @@ module todo.viewcontrols {
     export class TodoViewControl extends plat.ui.ViewControl {
         templateUrl = 'viewcontrols/todo/todo.viewcontrol.html';
         context = {
-            title: 'Todo List'
-        };
+		    newTodo: '',
+		    todos: []
+		};
+
+		createTodo() {
+		    var context = this.context,
+		        newTodo = {
+		        title: context.newTodo.trim(),
+		        completed: false,
+		        created: new Date()
+		    };
+
+		    context.todos.push(newTodo);
+		    context.newTodo = '';
+		}
       }
 
       plat.register.viewControl('todo', TodoViewControl);
